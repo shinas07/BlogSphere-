22943d2f316695c58fed3c24dfc47dfe9636b376
shinas07 <shinasamanpk@gmail.com>
2024-08-04 20:41:12 +0530

    Update UserService models

--- Backend/UserService/user/models.py
+++ Backend/UserService/user/models.py
@@ -1,14 +1,14 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
 
 
-from django.db import models
+# from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=100, null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    password = models.CharField(max_length=100, null=True, blank=True)
+# class User(models.Model):
+#     username = models.CharField(max_length=100, null=True, blank=True)
+#     email = models.EmailField(null=True, blank=True)
+#     password = models.CharField(max_length=100, null=True, blank=True)
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
