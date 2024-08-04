0852a5dc8a7b44038f13c5ebe638457ca2b9cb44
shinas07 <shinasamanpk@gmail.com>
2024-08-04 20:41:33 +0530

    Update frontend App component

--- Frontend/blog-frontend/src/App.jsx
+++ Frontend/blog-frontend/src/App.jsx
@@ -5,20 +5,25 @@ import Signup from './pages/SignUp';
 import Login from './pages/Login';
 import PrivateRoute from './pages/PrivateRoute';
 import Logout from './pages/LogOut';
-
+import ArticleDetail from './pages/ArticleDetail';
 
 function App() {
   return (
-    <Router>
+  <div className='h-screen'>
+      <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/articles" element={<PrivateRoute element={<Articles />} />} />
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path='/logout' element={<Logout/>}></Route>
+        <Route path="/article/:id" element={<ArticleDetail />} />
+
       </Routes>
     </Router>
+  </div>
   )
 }
+
 
 export default App
