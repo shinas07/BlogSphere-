b6f502937687e3e8aabb7afc20f7031512c75222
shinas07 <shinasamanpk@gmail.com>
2024-08-04 20:41:16 +0530

    Update UserService URLs

--- Backend/UserService/user/urls.py
+++ Backend/UserService/user/urls.py
@@ -1,10 +1,12 @@
 # user/urls.py
 from django.urls import path
 from .views import RegisterView, LoginView
-from .views import TokenRefreshView
+from .views import TokenRefreshView, verify_jwt
+from . import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify_jwt/',views.verify_jwt,name='varify_jwt'),
 ]
